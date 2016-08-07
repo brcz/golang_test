2d0cdef499f3b7b5cb02308fdf6451a9d09b152e
Konstantin Yevdokimov <brcz@nm.ru>
2016-08-07 15:41:10 +0300

    tests modifications

--- src/main_test.go
+++ src/main_test.go
@@ -12,22 +12,29 @@ type testPair struct {
 var tests = []testPair{
     {   "@chris you around?",
         `{"mentions":["chris"]}` },
-    
+
     {   "Good morning! (megusta) (coffee)",
         `{"emoticons":["megusta","coffee"]}` },
-    
+
     {   "Olympics are starting soon; http://www.nbcolympics.com",
         `{"links":[{"url":"http://www.nbcolympics.com","title":"2016 Rio Olympic Games | NBC Olympics"}]}` },
-    
+
     {   "@bob @john (success) such a cool feature; \nhttps://twitter.com/jdorfman/status/430511497475670016",
         `{"mentions":["bob","john"],"emoticons":["success"],"links":[{"url":"https://twitter.com/jdorfman/status/430511497475670016","title":"Justin Dorfman on Twitter: \"nice @littlebigdetail from @HipChat (shows hex colors when pasted in chat). http://t.co/7cI6Gjy5pq\""}]}` },
-    { "Mentions: @bob fail@bob word @bob  @ bob  Emoticons  (emoticon) (emoticonlongerthanthemaxlimit) (nested (emo)) (nonclosed", `{"mentions":["bob","bob","bob"],"emoticons":["emoticon","emo"]}` },
+
+    { "Mentions: @bob fail@bob word @bob  @ bob  ",
+			`{"mentions":["bob","bob","bob"]}` },
+
+	 { "Emoticons  (emoticon) (emoticonlongerthanthemaxlimit) (nested (emo)) (nonclosed",
+			`{"emoticons":["emoticon","emo"]}` },
+	{ "Links: www.google.com, http://google.com, (http://google.com)",
+			`{"links":[{"url":""}]}`},
 }
 
 func TestParseInput(t *testing.T) {
     for _, pair := range tests {
         result,_ := ParseInput(pair.sampleSource)
-        
+
         if result != pair.expectedResult {
             t.Error(
                     "For", pair.sampleSource,
@@ -36,5 +43,5 @@ func TestParseInput(t *testing.T) {
                     )
         }
     }
-    
+
 }
